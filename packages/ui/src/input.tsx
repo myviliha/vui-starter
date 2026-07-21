58859820fb7a9c8881f37789208ef445f5b7df06
Suman Bonakurthi <sumanbonakurthi@gmail.com>
2026-07-21 18:08:43 +0700

    bug fixed

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -14,7 +14,7 @@ export const Input = React.forwardRef<
       "placeholder:text-muted-foreground",
       "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:ring-offset-background",
       "disabled:cursor-not-allowed disabled:opacity-50",
-      "file:border-0 file:bg-transparent file:file:font-medium",
+      "file:border-0 file:bg-transparent file:font-medium",
       className,
     )}
     {...props}
